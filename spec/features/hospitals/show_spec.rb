f751cd0274a7cc0be39cd3eac0170957dbde2fbb
Sean Steel <seanrsteel@gmail.com>
2020-11-09 10:03:45 -0700

    Modify test setup for hospital show page

--- spec/features/hospitals/show_spec.rb
+++ spec/features/hospitals/show_spec.rb
@@ -7,7 +7,9 @@ RSpec.describe 'Hospital show page' do
       @hospital_1 = Hospital.create!(name: 'Grey Sloan Memorial Hospital')
       @hospital_2 = Hospital.create!(name: 'Seaside Health & Wellness Center')
       @doc_1 = @hospital_1.doctors.create!(name: 'Meredith Grey', specialty: 'General Surgery', education: 'Harvard University')
-      @doc_2 = @hospital_1.doctors.create!(name: 'Alex Karev', specialty: 'Pediatric Surgery', education: 'Johns Hopkins University')
+      @doc_2 = @hospital_1.doctors.create!(name: 'Alex Karev', specialty: 'Pediatric Surgery', education: 'Stanford University')
+      @doc_3 = @hospital_1.doctors.create!(name: 'Miranda Bailey', specialty: 'General Surgery', education: 'Stanford University')
+      @doc_4 = @hospital_2.doctors.create!(name: 'Derek McDreamy Shepherd', specialty: 'Attending Surgeon', education: 'University of Pennsylvania')
       @patient_1 = Patient.create!(name: 'Katie Bryce', age: 24)
       @patient_2 = Patient.create!(name: 'Denny Duquette', age: 39)
       @patient_3 = Patient.create!(name: 'Rebecca Pope', age: 32)
@@ -21,6 +23,10 @@ RSpec.describe 'Hospital show page' do
       visit "/hospitals/#{@hospital_1.id}"
     end
 
+    it 'see the hospitals name'
 
+    it 'see the number of doctors that work at this hospital'
+
+    it 'see a unique list of universities that the doctors attended'
   end
-end 
+end
